be7ac8e83b8761b98f9263c02bd22efb9ce82991
Lautaro Nuñez <Lautaro.Nunez@jala.university>
2025-07-19 12:04:35 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+on_off_app = True
+disp_data = {}
+
+while on_off_app:
+    print('''Bienvenido. Este es el menú inicial.
+          Elija el número de la siguiete lista de opciones 
+          1. Contestar encuesta.
+          2. Visualizar resultados. 
+          0. Apagar programa''')
+    
